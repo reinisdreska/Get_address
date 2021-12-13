fbc30eb6c62ab84554621ae8e69f00e6ac7dd510
reinis dreska <rdreska@gmail.com>
2021-12-13 17:15:55 +0200

    nav vairs static un templates folderi un pievienots stils

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/", methods=['GET', 'POST'])
+@cross_origin()
 def index():
     if request.method == "POST":
         raw_data = request.form["raw_data"]
         return raw_data
-    return render_template("index.html")
